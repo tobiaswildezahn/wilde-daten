94052da18b4f267a363aaaf0e0aa1db6828aa76e
tobiaswildezahn <tobias.zahn.hamburg@gmail.com>
2019-12-31 11:37:40 +0100

    second try

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import numpy as np 
+
+st.title("Ein Zufallsdiagramm")
+
+chart = st.bar_chart(np.random.rand(10,5))
+btn = st.button("Zufallsgenerator!")
+
+if btn:
+	chart.bar_chart(np.random.rand(10,5))
